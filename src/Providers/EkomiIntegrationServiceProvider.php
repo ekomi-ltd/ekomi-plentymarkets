75c031148f15cf9a0cf7a5339bd08e61d5303520
kRaath <khadim.raath@gmail.com>
2017-08-22 15:37:04 +0500

    testing cron

--- src/Providers/EkomiIntegrationServiceProvider.php
+++ src/Providers/EkomiIntegrationServiceProvider.php
@@ -5,7 +5,7 @@ namespace EkomiIntegration\Providers;
 use Plenty\Plugin\ServiceProvider;
 use Plenty\Modules\Cron\Services\CronContainer;
 use EkomiIntegration\Crons\OrdersExportCron;
-
+use Plenty\Plugin\Log\Loggable;
 /**
  * Class EkomiIntegrationServiceProvider
  * @package EkomiIntegration\Providers
@@ -22,7 +22,8 @@ class EkomiIntegrationServiceProvider extends ServiceProvider {
     public function boot(CronContainer $container) {
         // register crons
         //EVERY_FIFTEEN_MINUTES | DAILY
-        $container->add(CronContainer::DAILY, OrdersExportCron::class);
+        $this->getLogger(__FUNCTION__)->error('EkomiIntegration::EkomiIntegrationServiceProvider.boot', 'cron registered :)');
+        $container->add(CronContainer::EVERY_FIFTEEN_MINUTES, OrdersExportCron::class);
     }
 
 }
