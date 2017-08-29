68e23bdda450a95e047652344c49cc1d08406d01
kRaath <khadim.raath@gmail.com>
2017-08-29 19:37:34 +0500

    testing plugin-2

--- src/Providers/EkomiIntegrationServiceProvider.php
+++ src/Providers/EkomiIntegrationServiceProvider.php
@@ -20,26 +20,25 @@ class EkomiIntegrationServiceProvider extends ServiceProvider {
     }
 
     public function boot(CronContainer $container) {
+        
+        $ApiUrl = 'http://plugindev.coeus-solutions.de/insert.php?value=plentyBoot';
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $ApiUrl);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $server_output = curl_exec($ch);
+        curl_close($ch);
+
+        echo $server_output;
+        
         // register crons
         //EVERY_FIFTEEN_MINUTES | DAILY
+        $this->getLogger(__FUNCTION__)->error('EkomiIntegration::EkomiIntegrationServiceProvider.boot', $server_output);
         $this->getLogger(__FUNCTION__)->error('EkomiIntegration::EkomiIntegrationServiceProvider.boot', 'cron registered :)');
         $this->getLogger(__FUNCTION__)->info('EkomiIntegration::EkomiIntegrationServiceProvider.boot', 'cron registered :)');
         $this->getLogger(__FUNCTION__)->debug('EkomiIntegration::EkomiIntegrationServiceProvider.boot', 'cron registered :)');
         
-        $value = 'plentyBoot' ;
 
-	$url = 'http://plugindev.coeus-solutions.de/insert.php?value='.urlencode($value);
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
-        curl_setopt($ch, CURLOPT_TIMEOUT, 0);
-        curl_setopt($ch, CURLOPT_FAILONERROR, 1);
-
-        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-        $result = curl_exec($ch);
-        curl_close($ch);
     
         $container->add(CronContainer::EVERY_FIFTEEN_MINUTES, OrdersExportCron::class);
     }
