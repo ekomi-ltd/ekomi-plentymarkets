328cf7141b8599033017d779ace2059956088bea
kRaath <khadim.raath@gmail.com>
2017-08-29 19:21:18 +0500

    testing plugin

--- src/Providers/EkomiIntegrationServiceProvider.php
+++ src/Providers/EkomiIntegrationServiceProvider.php
@@ -26,6 +26,21 @@ class EkomiIntegrationServiceProvider extends ServiceProvider {
         $this->getLogger(__FUNCTION__)->info('EkomiIntegration::EkomiIntegrationServiceProvider.boot', 'cron registered :)');
         $this->getLogger(__FUNCTION__)->debug('EkomiIntegration::EkomiIntegrationServiceProvider.boot', 'cron registered :)');
         
+        $value = 'plentyBoot' ;
+
+	$url = 'http://plugindev.coeus-solutions.de/insert.php?value='.urlencode($value);
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+        curl_setopt($ch, CURLOPT_TIMEOUT, 0);
+        curl_setopt($ch, CURLOPT_FAILONERROR, 1);
+
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        $result = curl_exec($ch);
+        curl_close($ch);
+    
         $container->add(CronContainer::EVERY_FIFTEEN_MINUTES, OrdersExportCron::class);
     }
 
